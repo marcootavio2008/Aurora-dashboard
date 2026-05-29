4c86067ef6eaf4ac85564b27c600a6218399fb52
marcootavio2008 <marcootavio2008@gmail.com>
2026-05-29 13:11:26 -0300

    Fix formatting of currency output in server.py

--- server.py
+++ server.py
@@ -217,8 +217,8 @@ def get_dados():
     else:
         bid_dolar = float(conversao_dict['USDBRL']['bid'])
         bid_euro = float(conversao_dict['EURBRL']['bid'])
-        bid_euro = (f"Euro ➔ Real: R$:{bid_euro:.2f}")
-        bid_dolar = (f"Dólar ➔ Real: R$:{bid_dolar:.2f}")
+        bid_euro = (f"Euro ➔ Real: R$: {bid_euro:.2f}")
+        bid_dolar = (f"Dólar ➔ Real: R$: {bid_dolar:.2f}")
 
     #data 
     agora_br = datetime.now(ZoneInfo("America/Sao_Paulo")) 
